1d57c2d07fa0e11d514d9365a8539f6254f6408f
Richard  Oladeji <oladejir@accessbankplc.com>
2025-07-08 08:07:57 +0100

    after working on all the type error on the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,194 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import type { HeaderProp, MainPosts, PostProp, SearchPosts } from "./types";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
 }
 
-export default App
+function App() {
+  const [posts, setPosts] = useState<PostProp[]>(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post: PostProp) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
+
+  return (
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+
+      <Header
+        posts={searchedPosts}
+        onClearPosts={handleClearPosts}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <Main posts={searchedPosts} onAddPost={handleAddPost} />
+      <Archive onAddPost={handleAddPost} />
+      <Footer />
+    </section>
+  );
+}
+
+function Header({ posts, onClearPosts, searchQuery, setSearchQuery }:HeaderProp) {
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Atomic Blog
+      </h1>
+      <div>
+        <Results posts={posts} />
+        <SearchPosts
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+function SearchPosts({ searchQuery, setSearchQuery }: SearchPosts) {
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
+function Results({ posts }: {posts: PostProp[]}) {
+  return <p>🚀 {posts.length} atomic posts found</p>;
+}
+
+function Main({ posts, onAddPost }: MainPosts) {
+  return (
+    <main>
+      <FormAddPost onAddPost={onAddPost} />
+      <Posts posts={posts} />
+    </main>
+  );
+}
+
+function Posts({ posts }: {posts: PostProp[]}) {
+  return (
+    <section>
+      <List posts={posts} />
+    </section>
+  );
+}
+
+function FormAddPost({ onAddPost }: { onAddPost: (post: PostProp)=> void}) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = function (e: React.FormEvent) {
+    e.preventDefault();
+    if (!body || !title) return;
+    onAddPost({ title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Post title"
+      />
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        placeholder="Post body"
+      />
+      <button>Add post</button>
+    </form>
+  );
+}
+
+function List({ posts }: {posts: PostProp[]}) {
+  return (
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Archive({ onAddPost }: {onAddPost: (post: PostProp)=> void}) {
+  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+  const [posts] = useState(() =>
+    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+    Array.from({ length: 10000 }, () => createRandomPost())
+  );
+
+  const [showArchive, setShowArchive] = useState(false);
+
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+}
+
+function Footer() {
+  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+}
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type PostProp = {
+    title: string;
+    body: string
+}
+
+export type HeaderProp = {
+    posts: PostProp[];
+    onClearPosts: ()=> void;
+    searchQuery: string;
+    setSearchQuery: React.Dispatch<React.SetStateAction<string>>
+}
+
+
+export type SearchPosts = {
+    searchQuery: string;
+    setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export type MainPosts = {
+    posts: PostProp[];
+    onAddPost: (post: PostProp)=> void;
+}
